01725de819c9777b776876a1e0012bc057db0945
Sergey <puck2018s@gmail.com>
2025-04-07 12:32:32 +0300

    step 5

--- app.py
+++ app.py
@@ -41,8 +41,33 @@ def post_detail(id):
     article = Article.query.get(id)
     return render_template("post_detail.html", article=article)
 
+@app.route("/posts/<int:id>/del")
+def post_delete(id):
+    article = Article.query.get_or_404(id)
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect("/posts")
+    except:
+        return 'При удалении статьи произошла ошибка'
+    
+@app.route("/posts/<int:id>/update", methods=["POST", "GET"])
+def post_update(id):
+    article = Article.query.get(id)
+    if request.method == "POST":
+        article.title = request.form["title"]
+        article.intro = request.form["intro"]
+        article.text = request.form["text"]
+        try:
+            db.session.commit()
+            return redirect("/posts")
+        except:
+            return "При редактировании статьи произошла ошибка"
+    else:
+        return render_template("post_update.html", article=article)
+
 @app.route("/create-article", methods=["POST", "GET"])
-def create_artile():
+def create_article():
     if request.method == "POST":
         title = request.form["title"]
         intro = request.form["intro"]
@@ -53,7 +78,7 @@ def create_artile():
             db.session.commit()
             return redirect("/posts")
         except:
-            return "При добавление статьи произошла ошибка"
+            return "При добавлении статьи произошла ошибка"
     else:
         return render_template("create-article.html")
 
